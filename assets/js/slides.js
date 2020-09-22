14f70438a90f687dcf41beefa49b736d38230b4e
Kenneth <kennethvdstrieckt@hotmail.com>
2020-09-22 12:32:29 +0200

    slideshow tryout

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,58 +1,14 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
-
-try{
-  document.createEvent("TouchEvent");
-
-  // Here we're ensured touch capabilities
-  var scroll = document.getElementById('slideshow-swipe');
-  var start = null;
-  var clickIfEnd = true;
-  scroll.addEventListener('touchstart', function (e) {
-    start = e.touches[0].pageX;
-    clickIfEnd = true;
-    e.preventDefault();
-  }, false);
-  scroll.addEventListener('touchmove', function (e) {
-    this.scrollLeft = start - e.touches[0].pageX;
-    clickIfEnd = false;
-    e.preventDefault();
-  }, false);
-  scroll.addEventListener('touchend', function (e) {
-    if (clickIfEnd) {
-      if (e) {
-        clickIfEnd = e.target;
-        // Add a delay before treating this as a click, in case it was accidental
-        setTimeout(arguments.callee, 100);
-      } else {
-        // clickIfEnd is the element that was clicked!
-      }
-    }
-  }, false);
-} catch(e){}
